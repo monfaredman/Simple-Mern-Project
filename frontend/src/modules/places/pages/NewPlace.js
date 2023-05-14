66d3b9ef3ce3a1e929f8fd3ebb0d3c6bc5fcf105
moslem <monfaredman@outlook.com>
2023-05-14 15:56:57 +0330

    add

--- frontend/src/modules/places/pages/NewPlace.js
+++ frontend/src/modules/places/pages/NewPlace.js
@@ -1,5 +1,71 @@
-import React from "react";
+import React, { useCallback, useReducer } from "react";
+import Input from "../../../shared/components/FormElements/Input";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../../shared/util/validatiors";
+import "./NewPlace.css";
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_INPUT":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
+};
 
 export default function NewPlace() {
-  return <div>NewPlace</div>;
+  const inputHandler = useCallback((id, value, isValid) => {}, []);
+  const [state, dispatch] = useReducer(formReducer, {
+    Inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+
+  return (
+    <form className="place-form">
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        errorText="Please enter a valid title!"
+        validators={[VALIDATOR_REQUIRE()]}
+        onInput={inputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        type="text"
+        label="Description"
+        errorText="Please enter a valid description (at least 5 characters)!"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        onInput={inputHandler}
+      />
+    </form>
+  );
 }

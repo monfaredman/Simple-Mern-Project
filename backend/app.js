9abfba68af75e722e11170df4f7001daf966a2c8
monfaredman <monfaredman@outlook.com>
2023-06-24 18:14:26 +0330

    add

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,26 @@
 const express = require("express");
-const placeRoutes = require("./routes/places.js");
-const usersRoutes = require("./routes/users.js");
+// const placeRoutes = require("./routes/places.js");
+// const usersRoutes = require("./routes/users.js");
 const bodyParser = require("body-parser");
-const HttpError = require("./models/http-error");
+// const HttpError = require("./models/http-error");
+const mongoPractice = require("./mongo.js");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use("/api/places", placeRoutes);
-app.use("/api/users", usersRoutes);
-app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route.", 404);
-  throw error;
-});
-app.use((error, req, res, next) => {
-  if (res.headerSent) {
-    return next(error);
-  }
-  res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occured!" });
-});
+app.post("/products", mongoPractice.createProduct);
+// app.use("/api/places", placeRoutes);
+// app.use("/api/users", usersRoutes);
+// app.use((req, res, next) => {
+//   const error = new HttpError("Could not find this route.", 404);
+//   throw error;
+// });
+// app.use((error, req, res, next) => {
+//   if (res.headerSent) {
+//     return next(error);
+//   }
+//   res.status(error.code || 500);
+//   res.json({ message: error.message || "An unknown error occured!" });
+// });
 
 app.listen(5000); // start Node + Express server on port 5000

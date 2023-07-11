475ed4aa0decfb6ddc03dd1f8a2e6908330347f0
monfaredman <monfaredman@outlook.com>
2023-07-11 12:21:46 +0330

    add-netlify-deploy

---
+++ backend/functions/app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+const placeRoutes = require("../routes/places.js");
+const usersRoutes = require("../routes/users.js");
+const bodyParser = require("body-parser");
+const HttpError = require("../models/http-error.js");
+const mongoose = require("mongoose");
+const https = require("https");
+
+const app = express();
+
+// Middleware to redirect HTTP to HTTPS
+// const forceHttps = (req, res, next) => {
+//   if (!req.secure) {
+//     return res.redirect(`https://${req.headers.host}${req.url}`);
+//   }
+//   next();
+// };
+
+// app.use(forceHttps);
+app.use(bodyParser.json());
+app.use(
+  "/uploads/images",
+  express.static(path.join(__dirname, "uploads", "images"))
+);
+app.use(
+  cors({
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+    ],
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+  })
+);
+
+app.use("/api/places", placeRoutes);
+app.use("/api/users", usersRoutes);
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
+app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
+    });
+  }
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.e2p0gjl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("connected to database!");
+
+    const server = https.createServer(null, app);
+    server.listen(5000, () => {
+      console.log("Server running on port 5000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Export your app wrapped in serverless-http
+module.exports.handler = serverless(app);

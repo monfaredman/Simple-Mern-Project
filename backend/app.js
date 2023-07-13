c8ceec974fbb0a57701c721e9c918c7b33260fb9
monfaredman <monfaredman@outlook.com>
2023-07-13 12:28:21 +0330

    add

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,25 @@
 const express = require("express");
-const serverless = require("serverless-http");
 const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
 const placeRoutes = require("./routes/places.js");
 const usersRoutes = require("./routes/users.js");
 const bodyParser = require("body-parser");
-const HttpError = require("./models/http-error.js");
+const HttpError = require("./models/http-error");
 const mongoose = require("mongoose");
 const https = require("https");
 
 const app = express();
-const router = express.Router();
 
 // Middleware to redirect HTTP to HTTPS
-// const forceHttps = (req, res, next) => {
-//   if (!req.secure) {
-//     return res.redirect(`https://${req.headers.host}${req.url}`);
-//   }
-//   next();
-// };
+const forceHttps = (req, res, next) => {
+  if (!req.secure) {
+    return res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+  next();
+};
 
-// app.use(forceHttps);
-
+app.use(forceHttps);
 app.use(bodyParser.json());
 app.use(
   "/uploads/images",
@@ -41,13 +38,11 @@ app.use(
   })
 );
 
-app.use("/.netlify/functions/api", router);
-
 app.use("/api/places", placeRoutes);
 app.use("/api/users", usersRoutes);
 app.use((req, res, next) => {
   const error = new HttpError("Could not find this route.", 404);
-  next(error);
+  throw error;
 });
 app.use((error, req, res, next) => {
   if (req.file) {
@@ -61,6 +56,12 @@ app.use((error, req, res, next) => {
   res.status(error.code || 500);
   res.json({ message: error.message || "An unknown error occurred!" });
 });
+
+const options = {
+  key: fs.readFileSync("./private-key.pem"), // Update with your private key file path
+  cert: fs.readFileSync("./certificate.pem"), // Update with your certificate file path
+};
+
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.e2p0gjl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
@@ -68,7 +69,7 @@ mongoose
   .then(() => {
     console.log("connected to database!");
 
-    const server = https.createServer(null, app);
+    const server = https.createServer(options, app);
     server.listen(5000, () => {
       console.log("Server running on port 5000");
     });
@@ -76,6 +77,3 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-module.exports = app;
-module.exports.handler = serverless(app);

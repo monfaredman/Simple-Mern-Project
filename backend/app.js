7f780cc0ebad6d42bdf226bcf651c0606e9f1e9c
monfaredman <monfaredman@outlook.com>
2023-07-13 15:19:32 +0330

    change-port

--- backend/app.js
+++ backend/app.js
@@ -74,7 +74,7 @@ mongoose
     console.log("connected to database!");
 
     const server = https.createServer(options, app);
-    server.listen(5000, () => {
+    server.listen(process.env.PORT || 5000, () => {
       console.log("Server running on port 5000");
     });
   })
